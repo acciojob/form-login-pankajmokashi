3d7989c2cd988443d4a0da29ea238f5d7221d870
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-18 16:39:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let form = document.querySelectorAll("form > input")
+    let fname = form[0]
+    let lname = form[1]
 
+    alert(fname.value + " " + lname.value)
 }
